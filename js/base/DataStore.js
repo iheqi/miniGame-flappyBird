2031561f980793cce0c5c07f3877a4fbfa8a82a4
rubbish-man <993284758@qq.com>
2018-06-11 15:02:23 +0800

    flappyBird init

---
+++ js/base/DataStore.js
@@ -0,0 +1,4 @@
+// 变量缓存器   就像vuex？
+export class DataStore {
+	
+}
---
+++ js/base/ResourceLoader.js
@@ -0,0 +1,33 @@
+// ResourceLoader.js  资源文件加载器，确保canvas在图片资源加载完成后才进行渲染
+
+import { Resources } from './Resources.js'
+
+export class ResourceLoader {
+	
+	constructor() {
+		this.map = new Map(Resources)
+		for (let [key, value] of this.map) {
+			const image = new Image();
+			image.src = value;
+			this.map.set(key, image);
+		}
+	}
+
+	// 加载完毕函数
+	onLoaded(callback) {
+		let loadedCount = 0;
+
+		for (let value of this.map.values()) {
+			value.onload = () => {               // value: image对象
+				loadedCount++;
+				if (loadedCount >= this.map.size) {
+					callback(this.map);
+				}
+			}
+		}
+	}
+
+	static create() {
+		return new ResourceLoader();
+	}
+} 
